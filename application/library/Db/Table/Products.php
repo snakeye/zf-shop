ed4a8c536eaec5a3176b5148a1643786fde3f077
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2014-03-18 00:42:09 +0400

    Updated .gitignore

---
+++ application/library/Db/Table/Products.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Class Db_Table_Products
+ */
+class Db_Table_Products extends Zend_Db_Table
+{
+    protected $_name = 'products';
+    protected $_rowClass = 'Product';
+
+    /**
+     * Fetch single product
+     *
+     * @param $id
+     * @return null|Zend_Db_Table_Row_Abstract
+     */
+    public function fetchProduct($id)
+    {
+        $select = $this->select()->where('id = ?', $id);
+        return $this->fetchRow($select);
+    }
+
+    /**
+     * Fetch teasers for home page or related products
+     *
+     * @param int $count
+     * @param Product $except
+     * @return Zend_Db_Table_Rowset_Abstract
+     */
+    public function fetchTeasers($count = 3, $except = null)
+    {
+        $select = $this->select()->order('RAND()')->limit($count);
+
+        if ($except != null) {
+            $select->where('id <> ?', $except->id);
+        }
+
+        return $this->fetchAll($select);
+    }
+
+    /**
+     * @return Zend_Db_Table_Select
+     */
+    public function selectProducts()
+    {
+        $select = $this->select();
+
+        return $select;
+    }
+}
